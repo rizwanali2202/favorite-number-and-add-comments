4b208b6d16c0b97d931be8097ffc1e29ada5c586
Rizwan Ali <164035652+rizwanali2202@users.noreply.github.com>
2024-03-28 17:56:49 +0500

    you can used your favorite number and also add comments
    by using this program you can use your favorite number and find answer and also check how we can add comments in program during coding

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+let FavoriteNumber = 6;
+console.log(`${FavoriteNumber}, "is my favourite number"`);
+//exercise 10
+// we print our name and age via using variable
+let favouriteName = "Rizwan ali";
+let age = 34;
+console.log(`name = ${favouriteName} and age = ${age}`);
---
+++ app.ts
@@ -0,0 +1,9 @@
+let FavoriteNumber : number = 6;
+console.log(`${FavoriteNumber}, "is my favourite number"`);
+
+
+//exercise 10
+// we print our name and age via using variable
+let favouriteName : string = "Rizwan ali";
+let age : number = 34;
+console.log(`name = ${favouriteName} and age = ${age}`);
